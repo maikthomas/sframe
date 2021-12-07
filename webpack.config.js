c81ce92a333318d9b57fa4de83913e4c6ee6d05d
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2021-12-07 17:24:10 +0100

    build

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /Worker\.(c|m)?js$/i,
+        loader: 'worker-loader',
+        options: {
+          inline: 'no-fallback',
+          esModule: true,
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist_github'),
+    library: 'sframe',
+    libraryTarget: 'umd',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
