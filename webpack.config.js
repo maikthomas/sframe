d88dec6b0e858b23383354b48b9ab43ebe5ddcdb
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2021-12-07 17:38:16 +0100

    add build back

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist_github'),
     library: 'sframe',
     libraryTarget: 'umd',
   },
